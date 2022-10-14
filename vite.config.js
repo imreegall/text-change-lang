a46f83282221db199c02679c8708a257050aa7c5
imreegall <imreegall@gmail.com>
2022-10-14 11:09:46 +0300

    four commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/text-change-lang/',
+  publicPath: '/text-change-lang/',
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
